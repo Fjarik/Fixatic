156d6d60d7b189311e12da3b919c5f0437fce83a
Fjarik <jirkafalta@gmail.com>
2022-03-15 13:50:09 +0100

    Added MudBlazor

--- src/Fixatic/FixaticApp/Program.cs
+++ src/Fixatic/FixaticApp/Program.cs
@@ -1,6 +1,7 @@
 using FixaticApp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
