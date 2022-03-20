d7b6bbdfa0749ae89448b3f4db6afe8b594774c3
Fjarik <jirkafalta@gmail.com>
2022-03-20 13:18:48 +0100

    Added appsettings

--- src/Fixatic/FixaticApp/Program.cs
+++ src/Fixatic/FixaticApp/Program.cs
@@ -2,16 +2,42 @@ using FixaticApp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
+using Fixatic.Types;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var section = builder.Configuration.GetSection("ApplicationSettings");
+var appSettings = new ApplicationSettings();
+section.Bind(appSettings);
+
+var services = builder.Services;
+
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddMudServices();
+services.AddLogging();
+services.AddRazorPages();
+services.AddServerSideBlazor();
+
+services.AddAuthorization();
+services.AddHttpClient();
+services.AddHttpContextAccessor();
+
+services.Configure<ApplicationSettings>(section);
+
+services.AddSingleton<WeatherForecastService>();
+services.AddMudServices(x =>
+{
+    x.SnackbarConfiguration.PositionClass = MudBlazor.Defaults.Classes.Position.BottomLeft;
+    x.SnackbarConfiguration.PreventDuplicates = true;
+    x.SnackbarConfiguration.SnackbarVariant = MudBlazor.Variant.Filled;
+});
 
 var app = builder.Build();
+var settingOpt = app.Services.GetRequiredService<IOptions<ApplicationSettings>>();
+
+var asm = typeof(Program).Assembly;
+var version = asm.GetName()?.Version ?? new Version(1, 0, 0);
+settingOpt.Value.AppVersion = version;
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,12 +46,24 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+
+if (!string.IsNullOrWhiteSpace(settingOpt.Value.BasePath))
+{
+    app.UsePathBase(settingOpt.Value.BasePath);
+}
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
---
+++ src/Fixatic/Infrastructure/Fixatic.Types/ApplicationSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fixatic.Types
+{
+    public class ApplicationSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+
+        public Version AppVersion { get; set; } = new Version(1,0,0);
+
+        public string BasePath { get; set; } = "/";
+    }
+}
