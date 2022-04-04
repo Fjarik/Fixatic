ef7fab4b9024a596a619de8ba565f7b67b705e46
Fjarik <jirkafalta@gmail.com>
2022-04-05 00:05:13 +0200

    Added dark mode

---
+++ src/Fixatic/FixaticApp/Shared/MainLayout.razor.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using MudBlazor;
+using FixaticApp;
+using FixaticApp.Shared;
+
+namespace FixaticApp.Shared
+{
+	public partial class MainLayout
+	{
+		private bool _isDarkMode = false;
+		private void SwitchMode()
+		{
+			_isDarkMode = !_isDarkMode;
+		}
+	}
+}
